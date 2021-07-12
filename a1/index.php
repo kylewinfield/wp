3c5c158c81bfb63976c51596658a4218b4fadacd
kylewinfield <s3541867@student.rmit.edu.au>
2021-07-12 14:51:34 +1000

    Adding bg, adding web content and style.css

--- a1/index.php
+++ a1/index.php
@@ -9,29 +9,74 @@
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
     <script src='../wireframe.js'></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Aleo:ital@1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet">
+
+
   </head>
 
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+      <div>
+        <h1>Secret Barber -icon-</h1>
+      </div>
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <div>
+        <a href="index.php">Home</a>
+        <a href="shop.php">Services</a>
+        <a>Contact</a>
+      </div>
     </nav>
 
     <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
+      <article>
+        <div class="main">
+       
+          <h2>A SECRET TO THE WORLD</h2>
+          <p>
+            Let your curiosity get the best of you and find your way to Secret Barber, 
+            where the hidden tricks of the trade are implemented to give you a personally
+            crafted barber experience.
+          </p>
+
+          <img id="img1" src="arthur-humeau-Twd3yaqA2NM-unsplash.jpg" 
+          alt= "Man getting beard shaved" width="500" height="400"> 
+
+          </div>
+
+          <div class="main">
+          <h2>CUTS</h2>
+          <p>
+          
+          </p>
+        </div>
+
+        <div class="main">
+          <h2>WHAT OUR CUSTOMERS HAVE TO SAY</h2>
+          <p>
+          
+          </p>
+        </div>
+
+        <div class="main", id="contact">
+          <h2>CONTACT US</h2>
+          <p><img id="email" src="2099199.png" alt="Email Icon" width="15" height="15">
+            secret.barber@fakeemail.com</p>
+          <p><img id="phone" src="597177.png" alt="Phone icon" width="15" height="15">
+            +0123 456 789</p>
+ 
+        </div>
       </article>
     </main>
 
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Kyle Winfield, s3541867. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
