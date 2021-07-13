e70c23a66d497581cccf84c5f324f85c497fcb61
kylewinfield <s3541867@student.rmit.edu.au>
2021-07-13 21:16:36 +1000

    img urls again

--- a1/shop.php
+++ a1/shop.php
@@ -94,9 +94,9 @@
 
     <section class="foot" , id="contact">
       <h2>CONTACT US</h2>
-      <p><img id="email" src="2099199.png" alt="Email Icon" width="15" height="15">
+      <p><img id="email" src="../../media/2099199.png" alt="Email Icon" width="15" height="15">
         secret.barber@fakeemail.com</p>
-      <p><img id="phone" src="597177.png" alt="Phone icon" width="15" height="15">
+      <p><img id="phone" src="../../media/597177.png" alt="Phone icon" width="15" height="15">
         +0123 456 789</p>
 
     </section>
